c203dcea2a5768e132abc34d939078a2b087cf4b
cezarybednarz <cezary.bednarz@gmail.com>
2021-05-17 01:17:19 +0200

    addTopDef(s)

--- Interpreter.hs
+++ Interpreter.hs
@@ -5,6 +5,7 @@ import Data.Map.Lazy as LazyMap
 import Chococino.Abs 
 import Control.Monad.State
 import Control.Monad.Except
+import Memory
 
 -- types --
 
@@ -14,12 +15,8 @@ data Val = VInt Integer | VBool Bool | VString String | VFunc Env [Arg] Block | 
 type Loc = Int
 type Env = LazyMap.Map Ident [Loc]
 type Store = LazyMap.Map Loc Val
-
 type Scope = [Ident]
 type Scopes = [Scope]
-
-emptyScope = []
-initScopes = [emptyScope]
 
 data IntState = IntState {
   store :: Store,
@@ -51,8 +48,7 @@ type IM a = StateT IntState (ExceptT String IO) a
 runIM :: IM a -> IntState -> IO (Either String (a, IntState))
 runIM m st = runExceptT (runStateT m st)
 
--- helpers --
-alloc :: IM Loc
+-- memory management -- 
 
 alloc :: IM Loc
 alloc = do
@@ -68,13 +64,10 @@ free l = do
 
 updateStore :: Loc -> Val -> IM ()
 updateStore v x = modify $ \state -> state { 
-  store = Map.insert v x (store state)}
+  store = LazyMap.insert v x (store state)}
 
 getStore :: IM Store  
 getStore = gets store
-
-initFreeLocs :: [Loc]
-initFreeLocs = [1..2^16]
 
 getFreeLocs :: IM [Loc]
 getFreeLocs = gets freeLocs
@@ -82,24 +75,102 @@ getFreeLocs = gets freeLocs
 putFreeLocs :: [Loc] -> IM ()
 putFreeLocs freeLocs = modify $ \r -> r { freeLocs }
 
--- interpreter --
+getEnv :: IM Env
+getEnv = gets env
+
+putEnv :: Env -> IM ()
+putEnv env = modify $ \r -> r { env }
+
+modifyEnv :: (Env -> Env) -> IM ()
+modifyEnv f = getEnv >>= putEnv . f
+
+getScopes ::  IM Scopes
+getScopes = gets scopes
+
+putScopes :: Scopes -> IM ()
+putScopes scopes = modify $ \r -> r { scopes }
+
+modifyScopes :: (Scopes -> Scopes) -> IM ()
+modifyScopes f = getScopes >>= putScopes . f
+
+popScope :: IM Scope
+popScope = do
+  (scope:scopes) <- getScopes
+  putScopes scopes
+  return scope
+
+enterScope :: IM ()
+enterScope = modifyScopes (emptyScope:)
+
+leaveScope :: IM ()
+leaveScope = do
+  env <- getEnv
+  scope <- popScope
+  -- TODO: free scope vars
+  let scopeLocs = catMaybes $ map (flip lookupEnv env) scope
+  mapM_ free scopeLocs
+  let env' = foldr (\n e -> Map.update pop n e)env scope
+  putEnv env' where
+    pop :: [Loc] -> Maybe [Loc]
+    pop [] = Nothing
+    pop (x:xs) = Just xs
+
+createVar :: Name -> IM Loc
+createVar n = do
+     l <- alloc
+     modifyEnv (updateEnv n l)
+     modifyScopes (addLocal n)
+     return l
+
+addLocal :: Name -> Scopes -> Scopes
+addLocal n (h:t) =(n:h):t 
+addLocal n [] = []
+
+lookupEnv :: Name -> Env -> Maybe Loc
+lookupEnv n e = do
+  stack <- Map.lookup n e 
+  case stack of
+    [] -> Nothing
+    (l:_) -> return l
+  
+updateEnv :: Name -> Loc -> Env -> Env
+updateEnv n l = Map.insertWith (++) n [l] 
+
+getNameLoc :: Name -> IM Loc
+getNameLoc n =  do
+  env <- getEnv
+  let res  = lookupEnv n env
+  maybe (throwError $ unwords["Undefined var",n,"env is",show env]) return res
+  
+getVar :: Name -> IM Val
+getVar v =  do
+  loc <- getNameLoc v
+  store <- getStore 
+  let res  = Map.lookup loc store
+  maybe (throwError $ "Unallocated var"++ v) return res
+
+-- run interpreter --
 
 interpretProgram :: Program -> IO (Either String (Integer, IntState))
 interpretProgram (Program program) = do
-  runIM (exec decl) initState -- todo zmienić to (powinno evalować "main")
+  addTopDefs program >> runIM (evalExpr $ EApp (Ident "main")) initState
 
+-- TopDef --
 
-addTopDef :: TopDef -> IM Env
--- todo
-  
+addTopDef :: TopDef -> IM ()
+addTopDef (t,id,args,b) = do
+  n <- (t,args,b)
+  l <- createVar id
+  updateStore l n
 
-addTopDefs :: [TopDef] -> IM Env
--- todo
+addTopDefs :: [TopDef] -> IM ()
+addTopDefs [] = return ()
+addTopDefs ((t,id,args,b):ds) = addTopDef t id args b >> addTopDefs ds
 
 -- Expr --
 
 evalExpr :: Expr -> IM Val
--- todo
+
 
 
 
